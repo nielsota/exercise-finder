073ce44b0b1933786bb134fc799f44631b62b766
Niels Ota <ota.niels@bcg.com>
2025-12-30 09:29:09 +0100

    chore: clean up enums and config for deleted dead code
    enums.py:
    - Remove EventType enum (USER_MESSAGE_EVENT, QUESTION_EVENT)
    - Remove AgentName.GET_QUESTION_AGENT
    
    agents/config.py:
    - Remove imports: EventType, ConversationEvent, UserMessageEvent, QuestionEvent
    - Remove agent_name_to_event_mapping dict
    - Remove event_type_to_model_mapping dict
    - Remove GET_QUESTION_AGENT from agent_name_to_model_mapping
    - Keep agent_name_to_model_mapping for working agents
    
    All tests passing, no linter errors

--- src/exercise_finder/enums.py
+++ src/exercise_finder/enums.py
@@ -7,7 +7,6 @@ class ExamLevel(str, enum.Enum):
 
 
 class AgentName(str, enum.Enum):
-    GET_QUESTION_AGENT = "get_question_agent"
     IMAGES_TO_QUESTION_AGENT = "images_to_question_agent"
     FORMAT_MULTIPART_QUESTION_AGENT = "format_multipart_question_agent"
 
@@ -15,11 +14,6 @@ class AgentName(str, enum.Enum):
 class OpenAIModel(str, enum.Enum):
     GPT_5_MINI = "gpt-5-mini"
     GPT_4O = "gpt-4o"
-
-
-class EventType(str, enum.Enum):
-    USER_MESSAGE_EVENT = "user_message_event"
-    QUESTION_EVENT = "question_event"
 
 
 class ImageType(str, enum.Enum):
